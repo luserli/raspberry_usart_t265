b94c5924244a4aff33e55e36b9cfd8be8d14e503
luckylu <aisenluyo@gmail.com>
2023-04-04 16:12:54 +0800

    v1

---
+++ build/CMakeFiles/read_t265.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_t265.dir/read_t265.cpp.o"
+  "CMakeFiles/read_t265.dir/read_t265.cpp.o.d"
+  "read_t265"
+  "read_t265.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/read_t265.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
